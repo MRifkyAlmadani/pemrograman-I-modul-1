91dac36b419963a9146b353c44a5bc8a2291c60c
MRifkyAlmadani <2510817310018@mhs.ulm.ac.id>
2025-10-14 14:18:35 +0800

    Add files via upload

---
+++ PRAK108-2510817310018-M. RIFKY ALMADANI.py
@@ -0,0 +1,11 @@
+putaran = 5
+jarak_tempuh = 14
+keliling = jarak_tempuh / putaran
+jari_jari = keliling / (2 * 3.14)
+
+print("Diketahui: ")
+print("Pak Dengklek mengelilingi taman = %d putaran" % putaran)
+print("Jarak tempuh Pak Dengklek = %d Kilometer" % jarak_tempuh)
+print()
+print("Jawaban: ")
+print("Jari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer" % jari_jari)
---
+++ PRAK109-2510817310018-M. RIFKY ALMADANI.py
@@ -0,0 +1,7 @@
+jumlah_pasukan = 958730
+jumlah_pahlawan = 5
+pembagian_musuh = jumlah_pasukan / jumlah_pahlawan
+
+print("Jumlah pasukan yang dibawa Yu Zhong = %d" %jumlah_pasukan)
+print("Jumlah pahlawan = %d" %jumlah_pahlawan)
+print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah %d" %pembagian_musuh)
